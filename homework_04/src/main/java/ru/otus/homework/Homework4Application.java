1d8866856b3b4b022a8d88d571acb37f9f9bd497
Илья Маслёнченко <ilnikmas@tut.by>
2022-05-02 14:44:01 +0300

    Homework_04

---
+++ homework_04/src/main/java/ru/otus/homework/Homework4Application.java
@@ -0,0 +1,11 @@
+package ru.otus.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework4Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Homework4Application.class, args);
+	}
+}
